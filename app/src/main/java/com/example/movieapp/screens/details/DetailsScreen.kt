ea5721a9e00e9129e8d82d4cc6d0c90bdc86c8ce
Ulises Cadena <ucadena07@gmail.com>
2023-11-02 06:41:30 -0500

    navigating back

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -2,14 +2,18 @@ package com.example.movieapp.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -19,6 +23,10 @@ fun DetailsScreen(navController: NavController, movieData: String?){
         .fillMaxHeight()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = { navController.popBackStack() }) {
+                Text(text ="Go Back")
+            }
 
         }
     }
