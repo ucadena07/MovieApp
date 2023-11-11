ba861e3c56fd5702834268b413116c40ddf642da
Ulises Cadena <ucadena07@gmail.com>
2023-11-11 08:56:22 -0600

    adding expand capability

--- app/src/main/java/com/example/movieapp/screens/widgets/widgets.kt
+++ app/src/main/java/com/example/movieapp/screens/widgets/widgets.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.screens.widgets
 
 import android.graphics.Paint.Style
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,6 +15,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,12 +24,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
@@ -37,11 +50,14 @@ import com.example.movieapp.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies().first(), onItemClick: (Movie) -> Unit = {}) {
+    var expanded by remember{
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+            //.height(130.dp)
             .clickable {
                 onItemClick(movie)
             },
@@ -70,8 +86,26 @@ fun MovieRow(movie: Movie = getMovies().first(), onItemClick: (Movie) -> Unit = 
                 Text(text = movie.title, style = MaterialTheme.typography.headlineSmall)
                 Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.titleSmall)
                 Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.titleSmall)
-            }
+               AnimatedVisibility(visible = expanded) {
+                   Column() {
+                      Text(buildAnnotatedString {
+                          withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)){
+                              append("Plot: ")
+                          }
+                          withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Bold)){
+                              append(movie.plot)
+                          }
+                      })
+                   }
+               }
 
+                Icon(imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown , contentDescription = "down arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        }, tint = Color.DarkGray)
+            }
         }
 
     }
